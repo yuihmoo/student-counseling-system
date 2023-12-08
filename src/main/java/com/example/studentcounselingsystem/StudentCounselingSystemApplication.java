e11b750f4b68e1c2207fe775eb50b81032bd3c5c
yuihmoo <yuihmoo@gmail.com>
2023-12-08 19:24:30 +0900

    project init

---
+++ src/main/java/com/example/studentcounselingsystem/StudentCounselingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.studentcounselingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentCounselingSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentCounselingSystemApplication.class, args);
+    }
+
+}
