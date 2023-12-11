eda736e4cfe9884c329986b48cec87a3e12198e8
yuihmoo <yuihmoo@gmail.com>
2023-12-11 14:06:57 +0900

    feat. 데이터베이스 create.sql, init.sql 추가

---
+++ src/main/resources/schema/create.sql
@@ -0,0 +1,45 @@
+CREATE DATABASE `student_counseling_system`;
+CREATE USER 'yuihmoo'@'192.168.%.1' IDENTIFIED BY 'ZHlkZWhmMTI=';
+GRANT ALL PRIVILEGES ON student_counseling_system.* TO 'yuihmoo'@'192.168.%.1';
+FLUSH PRIVILEGES;
+
+/*
+ 상담 테이블
+ */
+CREATE TABLE student_counseling_system.counseling
+(
+    `id`        BINARY(16) DEFAULT (UUID_TO_BIN(UUID(),1)) PRIMARY KEY,
+    studentId   BINARY(16),
+    content     VARCHAR(1000),
+    counselorId BINARY(16),
+    feedback    VARCHAR(1000),
+    isRead      BOOLEAN,
+    createdDate TIMESTAMP
+) ENGINE = INNODB
+  DEFAULT CHARSET = utf8mb4;
+
+/*
+ 학생 테이블
+ status (0 = 재원, 1 = 퇴원)
+ */
+CREATE TABLE student_counseling_system.student
+(
+    `id`     BINARY(16) DEFAULT (UUID_TO_BIN(UUID(),1)) PRIMARY KEY,
+    `name`   VARCHAR(1000),
+    `status` CHAR(1)
+) ENGINE = INNODB
+  DEFAULT CHARSET = utf8mb4;
+
+/*
+ 직원 테이블
+ type (C = Counselor 상담자, M = Manager 담당자)
+ status (0 = 재직, 1 = 퇴직)
+ */
+CREATE TABLE student_counseling_system.employee
+(
+    `id`     BINARY(16) DEFAULT (UUID_TO_BIN(UUID(),1)) PRIMARY KEY,
+    `name`   VARCHAR(1000),
+    `type`   CHAR(1),
+    `status` CHAR(1)
+) ENGINE = INNODB
+  DEFAULT CHARSET = utf8mb4;
---
+++ src/main/resources/schema/init.sql
@@ -0,0 +1,11 @@
+INSERT INTO student_counseling_system.student (name, status) VALUES ('학생1', '0');
+INSERT INTO student_counseling_system.student (name, status) VALUES ('학생2', '0');
+INSERT INTO student_counseling_system.student (name, status) VALUES ('학생3', '0');
+INSERT INTO student_counseling_system.student (name, status) VALUES ('학생4', '1');
+INSERT INTO student_counseling_system.student (name, status) VALUES ('학생5', '1');
+
+INSERT INTO student_counseling_system.employee (name, type, status) VALUES ('직원1', 'C', '0');
+INSERT INTO student_counseling_system.employee (name, type, status) VALUES ('직원2', 'C', '0');
+INSERT INTO student_counseling_system.employee (name, type, status) VALUES ('직원3', 'C', '0');
+INSERT INTO student_counseling_system.employee (name, type, status) VALUES ('직원4', 'M', '0');
+INSERT INTO student_counseling_system.employee (name, type, status) VALUES ('직원5', 'M', '0');
