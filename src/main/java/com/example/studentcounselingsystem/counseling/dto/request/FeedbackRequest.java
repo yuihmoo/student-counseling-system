2f9510ce33c9312f1316e3c62f7f34135ef55784
yuihmoo <yuihmoo@gmail.com>
2023-12-11 20:10:52 +0900

    feat. FeedbackRequest Validation 추가

--- src/main/java/com/example/studentcounselingsystem/counseling/dto/request/FeedbackRequest.java
+++ src/main/java/com/example/studentcounselingsystem/counseling/dto/request/FeedbackRequest.java
@@ -1,6 +1,8 @@
 package com.example.studentcounselingsystem.counseling.dto.request;
 
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Getter;
+import org.hibernate.validator.constraints.Length;
 
 import java.util.UUID;
 
@@ -8,5 +10,7 @@ import java.util.UUID;
 public class FeedbackRequest {
     private UUID id;
     private UUID counselorId;
+    @NotEmpty()
+    @Length(min = 50, max = 1000, message = "상담 내용은 최소 50자 이상 최대 1000자까지 입니다.")
     private String feedback;
 }
